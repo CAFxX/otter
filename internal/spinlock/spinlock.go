b815cf1d74ca0b62a842ef1c7d36299347d1b8b4
Carlo Alberto Ferraris <cafxx@strayorange.com>
2023-12-24 07:31:23 +0900

    Optimize spinlock.go

--- internal/spinlock/spinlock.go
+++ internal/spinlock/spinlock.go
@@ -8,30 +8,32 @@ import (
 const maxSpins = 16
 
 // SpinLock is an implementation of spinlock.
-type SpinLock uint32
+type SpinLock struct {
+	v atomic.Bool
+}
 
 // Lock locks sl. If the lock is already in use, the calling goroutine blocks until the spinlock is available.
 func (sl *SpinLock) Lock() {
 	spins := 0
 	for {
-		for atomic.LoadUint32((*uint32)(sl)) == 1 {
+		for sl.v.Load() {
 			spins++
 			if spins > maxSpins {
 				spins = 0
 				runtime.Gosched()
+			} else {
+				// TODO: add a PAUSE instruction
 			}
 		}
 
-		if atomic.CompareAndSwapUint32((*uint32)(sl), 0, 1) {
+		if !sl.v.Swap(true) {
 			return
 		}
-
-		spins = 0
 	}
 }
 
 // Unlock unlocks sl. A locked SpinLock is not associated with a particular goroutine.
 // It is allowed for one goroutine to lock a SpinLock and then arrange for another goroutine to unlock it.
 func (sl *SpinLock) Unlock() {
-	atomic.StoreUint32((*uint32)(sl), 0)
+	sl.v.Store(false)
 }
